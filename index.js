4e8a52f3fc912437485dbebc26c9793925ce7084
Chiao-Han Chang <mandy93142@gmail.com>
2024-02-01 05:28:32 +0800

    main menu function in index.js

--- index.js
+++ index.js
@@ -14,3 +14,51 @@ connection.connect(err => {
   if (err) throw err;
 });
 
+// Main menu function
+function mainMenu() {
+    inquirer.prompt({
+      name: 'action',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'View All Departments',
+        'View All Roles',
+        'View All Employees',
+        'Add a Department',
+        'Add a Role',
+        'Add an Employee',
+        'Update an Employee Role',
+        'Exit'
+      ]
+    })
+    .then(answer => {
+      switch (answer.action) {
+        case 'View All Departments':
+          viewDepartments();
+          break;
+        case 'View All Roles':
+          viewRoles();
+          break;
+        case 'View All Employees':
+          viewEmployees();
+          break;
+        case 'Add a Department':
+          addDepartment();
+          break;
+        case 'Add a Role':
+          addRole();
+          break;
+        case 'Add an Employee':
+          addEmployee();
+          break;
+        case 'Update an Employee Role':
+          updateEmployeeRole();
+          break;
+        case 'Exit':
+          connection.end();
+          break;
+      }
+    });
+  }
+
+  
