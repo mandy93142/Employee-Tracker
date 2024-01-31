d039dbaa6c70be23c68fe9f8eeb12fc51c4625e7
Chiao-Han Chang <mandy93142@gmail.com>
2024-02-01 07:48:20 +0800

    more functions in index.js

--- index.js
+++ index.js
@@ -61,4 +61,149 @@ function mainMenu() {
     });
   }
 
+  // View departments
+function viewDepartments() {
+    const query = 'SELECT * FROM department';
+    connection.query(query, (err, res) => {
+      if (err) throw err;
+      consoleTable(res);
+      mainMenu();
+    });
+  }
+
+  // Add department
+function addDepartment() {
+    inquirer.prompt({
+      name: 'newDepartment',
+      type: 'input',
+      message: 'What is the name of the new department?'
+    })
+    .then(answer => {
+      const query = 'INSERT INTO department (name) VALUES (?)';
+      connection.query(query, answer.newDepartment, (err, res) => {
+        if (err) throw err;
+        console.log('Department added successfully!');
+        mainMenu();
+      });
+    });
+  }
+
+  // View roles
+function viewRoles() {
+    const query = 'SELECT role.id, role.title, department.name AS department, role.salary FROM role INNER JOIN department ON role.department_id = department.id';
+    connection.query(query, (err, res) => {
+      if (err) throw err;
+      consoleTable(res);
+      mainMenu();
+    });
+  }
+  
+  // View employees
+  function viewEmployees() {
+    const query = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, \' \', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id';
+    connection.query(query, (err, res) => {
+      if (err) throw err;
+      consoleTable(res);
+      mainMenu();
+    });
+  }
+
+  // Add department
+function addDepartment() {
+    inquirer.prompt({
+      name: 'newDepartment',
+      type: 'input',
+      message: 'What is the name of the new department?'
+    })
+    .then(answer => {
+      const query = 'INSERT INTO department (name) VALUES (?)';
+      connection.query(query, answer.newDepartment, (err, res) => {
+        if (err) throw err;
+        console.log('Department added successfully!');
+        mainMenu();
+      });
+    });
+  }
+  
+  // Add role
+  function addRole() {
+    connection.query('SELECT * FROM department', (err, departments) => {
+      if (err) throw err;
+      inquirer.prompt([
+        {
+          name: 'title',
+          type: 'input',
+          message: 'What is the title of the new role?'
+        },
+        {
+          name: 'salary',
+          type: 'input',
+          message: 'What is the salary for this role?',
+          validate: value => !isNaN(value)
+        },
+        {
+          name: 'department',
+          type: 'list',
+          choices: departments.map(department => ({ name: department.name, value: department.id })),
+          message: 'Which department does this role belong to?'
+        }
+      ])
+      .then(answer => {
+        const query = 'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)';
+        connection.query(query, [answer.title, answer.salary, answer.department], (err, res) => {
+          if (err) throw err;
+          console.log('Role added successfully!');
+          mainMenu();
+        });
+      });
+    });
+  }
+  
+  // Add employee
+  function addEmployee() {
+    let roles, employees;
+    connection.promise().query('SELECT id, title FROM role')
+      .then(([rows]) => {
+        roles = rows;
+        return connection.promise().query('SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee');
+      })
+      .then(([rows]) => {
+        employees = rows;
+        return inquirer.prompt([
+          {
+            name: 'firstName',
+            type: 'input',
+            message: 'What is the employee\'s first name?'
+          },
+          {
+            name: 'lastName',
+            type: 'input',
+            message: 'What is the employee\'s last name?'
+          },
+          {
+            name: 'role',
+            type: 'list',
+            choices: roles.map(role => ({ name: role.title, value: role.id })),
+            message: 'What is the employee\'s role?'
+          },
+          {
+            name: 'manager',
+            type: 'list',
+            choices: [{ name: 'None', value: null }].concat(employees.map(employee => ({ name: employee.name, value: employee.id }))),
+            message: 'Who is the employee\'s manager?'
+          }
+        ]);
+      })
+      .then(answer => {
+        const query = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
+        connection.query(query, [answer.firstName, answer.lastName, answer.role, answer.manager], (err, res) => {
+          if (err) throw err;
+          console.log('Employee added successfully!');
+          mainMenu();
+        });
+      })
+      .catch(err => {
+        throw err;
+      });
+  }
   
